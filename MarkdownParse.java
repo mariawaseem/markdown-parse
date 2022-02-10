5f7ba250840139c4f2dc52f53d4275a87eb4b218
mariawaseem <97696497+mariawaseem@users.noreply.github.com>
2022-02-10 11:39:26 -0800

    changes

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,6 +17,9 @@ public class MarkdownParse {
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+
+            //System.out.println(currentIndex);
+            System.out.println(closeParen);
         }
         return toReturn;
     }
